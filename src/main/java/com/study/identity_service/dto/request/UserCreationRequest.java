7844f5f5e0bd1fea9e25309960c20a10ba6d63af
himeki204 <sonhz123@gmail.com>
2025-01-08 16:50:07 +0700

    Init

--- src/main/java/com/study/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/study/identity_service/dto/request/UserCreationRequest.java
@@ -1,9 +1,11 @@
 package com.study.identity_service.dto.request;
 
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
 import java.time.LocalDate;
 
+@Data
 public class UserCreationRequest {
     @Size(min = 4, max = 20, message = "USERNAME_INVALID")
     private String username;
@@ -14,43 +16,4 @@ public class UserCreationRequest {
     private String lastName;
     private LocalDate dob;
 
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/com/study/identity_service/dto/request/UserUpdateRequest.java
+++ src/main/java/com/study/identity_service/dto/request/UserUpdateRequest.java
@@ -1,42 +1,19 @@
 package com.study.identity_service.dto.request;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
 import java.time.LocalDate;
+import java.util.logging.Level;
 
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserUpdateRequest {
-    private String password;
-    private String firstName;
-    private String lastName;
-    private LocalDate dob;
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    String password;
+    String firstName;
+    String lastName;
+    LocalDate dob;
 }
